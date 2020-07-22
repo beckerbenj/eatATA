0e029849924b6ab6f7a2fb7ac93e0cd558183d11
Ben <benj.becker@web.de>
2020-07-22 10:37:31 +0200

    0.5.3
    docu fixes

--- R/items.R
+++ R/items.R
@@ -8,7 +8,7 @@
 #'\describe{
 #' \item{Item_ID}{Item identifier.}
 #' \item{exclusions}{Items which can not be in the same test form.}
-#' \item{RT_in_min}{Average response times in minutes.}
+#' \item{RT_in_min}{Average response times in minutes. \code{2.5} equals 2 minutens and 30 seconds, for example.}
 #' \item{subitems}{Number of sub items.}
 #' \item{MC, CMC, short_answer, open}{Answer formats.}
 #' \item{diff_1, diff_2, diff_3, diff_4, diff5}{Difficulty categories.}
