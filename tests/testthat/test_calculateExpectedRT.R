fda1fad5050cd376659ec495d477c9578de32754
Ben <benj.becker@web.de>
2020-11-30 17:08:26 +0100

    travis fix 1

--- tests/testthat/test_calculateExpectedRT.R
+++ tests/testthat/test_calculateExpectedRT.R
@@ -4,3 +4,9 @@ test_that("normal", {
   out <- calculateExpectedRT(4, phi = 1, zeta = 0, sdEpsi = 0.5)
   expect_equal(out[1], c(61.86781))
 })
+
+test_that("vectorized", {
+  out <- calculateExpectedRT(c(4, 5), phi = c(1, 1), zeta = 0, sdEpsi = c(0.5, 0.5))
+  expect_equal(out[1], c(61.86781))
+  expect_equal(out[2], c(168.1741))
+})
