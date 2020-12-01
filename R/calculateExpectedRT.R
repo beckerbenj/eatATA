24d5f578207b51a6013aac298c199aec5a09fa14
Benjamin Becker <benj.becker@web.de>
2020-12-01 17:26:20 +0100

    tests and docu calculateExpectedRT

--- R/calculateExpectedRT.R
+++ R/calculateExpectedRT.R
@@ -3,20 +3,43 @@
 #'
 #' Calculate expected response times given item parameters of the log normal response time model.
 #'
-#' Expected response times are calculated according to the log normal response time models by
+#' Expected response times are calculated according to the log normal response time model by
 #' van der Linden (2006) or Klein Entink et al. (2009). If \code{phi} is \code{1}, the model
 #' by van der Linden (2006) is used. Either a single set of parameters of vectors of each parameters
 #' can be supplied. If multiple \code{zeta} are supplied, a list
+#'
+#' The calculation is based on Fenton (1960). For the model by van der Linden (2006), the calculation was
+#' first introduced by van der Linden (2011).
 #'
 #'@param lambda Vector of time intensity parameters.
 #'@param phi Vector of time sensitivity parameters.
 #'@param zeta Vector of person speed parameters.
 #'@param sdEpsi Vector of item specific residual variances.
 #'
-#'@return a vector
+#'@return a matrix, with columns for different \code{zeta} and rows for different items
+#'
+#' @references Fenton, L. (1960). The sum of log-normal probability distributions in scatter transmission systems. \emph{IRE
+#' Transactions on Communication Systems}, 8, 57-67.
+#'
+#' Klein Entink, R. H., Fox, J.-P., & van der Linden, W. J. (2009). A multivariate
+#'multilevel approach to the modeling of accuracy and speed of test
+#'takers. \emph{Psychometrika}, 74(1), 21-48.
+#'
+#'van der Linden, W. J. (2006). A lognormal model for response times on test
+#'items. \emph{Journal of Educational and Behavioral Statistics, 31(2),
+#'181-204}.
+#'
+#'van der Linden, W. J. (2011). Test design and speededness. \emph{Journal of
+#'Educational Measurement}, 48(1), 44-60.
 #'
 #'@export
 calculateExpectedRT <- function(lambda, phi, zeta, sdEpsi) {
+  if(!is.numeric(lambda)) stop("'lambda' must be a numeric vector.")
+  if(!is.numeric(phi)) stop("'phi' must be a numeric vector.")
+  if(!is.numeric(zeta)) stop("'zeta' must be a numeric vector.")
+  if(!is.numeric(sdEpsi)) stop("'sdEpsi' must be a numeric vector.")
+  if(length(sdEpsi) != length(lambda) || length(sdEpsi) != length(phi)) stop("'lambda', 'phi', and 'sdEpsi' must be of the same length.")
+
   cumulants <- getCumulantRT(zeta, lambda, phi, sdEpsi)
   return(t(cumulants[[1]]))
 }
