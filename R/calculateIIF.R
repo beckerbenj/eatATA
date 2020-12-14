568ac8d2d9eff591b8fecec2b2ea2d63aa2fc8ab
ddebeer <debeer.dries@gmail.com>
2020-12-14 14:30:20 +0100

    D as argument in calculateIIF

--- R/calculateIIF.R
+++ R/calculateIIF.R
@@ -6,7 +6,8 @@
 #'@param theta Vector of time intensity parameters.
 #'@param A Vector of discrimination parameters.
 #'@param B Vector of difficulty parameters.
-#'@param C Vector of guessing parameters.
+#'@param C Vector of pseudo-guessing parameters.
+#'@param D the constant that should be used. Defaults to 1.7.
 #'
 #'@return a matrix, with columns for different \code{theta} and rows for different items
 #'
@@ -19,27 +20,33 @@
 #'# TIF for multiple items (1PL model)
 #'calculateIIF(B = c(1.1, 0.8, 0.5), theta = 0)
 #'
-#'# TIF for multiple items and multiple ability levels (1PL model)
+#'# TIF for multiple theta-values (3PL model)
+#'calculateIIF(B = -0.5, C = 0.25, theta = c(-1, 0, 1))
+#'
+#'# TIF for multiple items and multiple ability levels (2PL model)
 #'calculateIIF(A = c(0.7, 1.1, 0.8), B = c(1.1, 0.8, 0.5),
 #'             theta = c(-1, 0, 1))
 #'
 #'@export
-calculateIIF <- function(A = rep(1, length(B)), B, C = rep(0, length(B)), theta) {
+calculateIIF <- function(A = rep(1, length(B)), B, C = rep(0, length(B)), theta, D = 1.7) {
   if(!is.numeric(theta)) stop("'theta' must be a numeric vector.")
   if(!is.numeric(A)) stop("'A' must be a numeric vector.")
   if(!is.numeric(B)) stop("'B' must be a numeric vector.")
   if(!is.numeric(C)) stop("'C' must be a numeric vector.")
   if(length(A) != length(B) || length(A) != length(C)) stop("'A', 'B', and 'C' must be of the same length.")
 
-  J <- length(theta)
-  Info <- array(0,c(length(A),J))
-  for(j in 1:J){
-    P <- C+(1-C)/(1+exp(-1.7*A*(theta[j]-B))) ## Prob of correct response
-    Q <- 1-P
-    Info[,j] <- (1.7^2)*(A^2)*((P-C)/(1-C))^2*Q/P ## item information for corresponding theta value??
-  }
-  colnames(Info) <- paste0("theta=", theta)
-  Info
-}
+  # matrix form A and C
+  nP <- length(theta)
+  Am <- rep(D, nP) %o% A
+  Cm <- rep(1, nP) %o% C
 
+  # compute probabilities
+  P <- Cm + (1 - Cm) / (1 + exp(- Am * outer(theta, B, "-")))
+
+  # compute IIF
+  IIFs <- t(Am^2 * ((P - Cm)/(1 - Cm))^2 * (1 - P)/P)
+
+  colnames(IIFs) <- paste0("theta=", theta)
+  return(IIFs)
+}
 
