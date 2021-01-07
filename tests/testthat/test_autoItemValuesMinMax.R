05a0e45a979ca39ac2e49e833051fd9f84d39090
ddebeer <debeer.dries@gmail.com>
2021-01-07 17:41:43 +0100

    autoItemValuesMinMax.R

--- tests/testthat/test_autoItemValuesMinMax.R
+++ tests/testthat/test_autoItemValuesMinMax.R
@@ -1,20 +1,22 @@
 
 test_that("auto item value", {
   out <- autoItemValuesMinMax(nForms = 2, itemValues = c(1, 1, 1, 1), allowedDeviation = 0, verbose = FALSE)
-  expect_equal(out[1, ], c(1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 2))
-  expect_equal(out[4, ], c(0, 0, 0, 0, 1, 1, 1, 1, 0, -1, 2))
+  expect_equal(out$A_binary[1, ], c(1, 1, 1, 1, 0, 0, 0, 0))
+  expect_equal(out$A_binary[4, ], c(0, 0, 0, 0, 1, 1, 1, 1))
+  expect_equal(out$d, c(min = 2 , min = 2, max = 2, max = 2))
 
   out <- autoItemValuesMinMax(nForms = 2, itemValues = c(1, 1, 1, 1), allowedDeviation = .5, verbose = FALSE)
-  expect_equal(out[1, ], c(1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1.5))
-  expect_equal(out[4, ], c(0, 0, 0, 0, 1, 1, 1, 1, 0, -1, 2.5))
+  expect_equal(out$d, c(min = 1.5, min = 1.5, max = 2.5, max = 2.5))
+  expect_equal(out$operators, c(">=", ">=", "<=", "<="))
 
   out <- autoItemValuesMinMax(nForms = 2, itemValues = c(1, 1, 1, 1), verbose = FALSE)
-  expect_equal(out[1, ], c(1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 2))
-  expect_equal(out[2, ], c(0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 2))
+  expect_equal(out$d, c(2, 2))
+  expect_equal(out$operators, c("=", "="))
 
   out <- autoItemValuesMinMax(nForms = 2, itemValues = c(1, 1, 1, 1, 1), allowedDeviation = .5, verbose = FALSE)
-  expect_equal(out[1, ], c(1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 2))
-  expect_equal(out[4, ], c(0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, -1, 3))
+  expect_equal(out$A_binary[1, ], c(1, 1, 1, 1, 1, 0, 0, 0, 0, 0))
+  expect_equal(out$d, c(min = 2, min = 2, max = 3, max = 3))
+  expect_equal(out$operators, c(">=", ">=", "<=", "<="))
 
   expect_equal(autoItemValuesMinMax(nForms = 2, itemValues = c(1, 1, 1, 1), allowedDeviation = 1, verbose = FALSE),
                autoItemValuesMinMax(nForms = 2, itemValues = c(1, 1, 1, 1), allowedDeviation = .5, relative = TRUE, verbose = FALSE))
