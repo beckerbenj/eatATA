80eb762317216256d8ec56e67008f5daaf971421
ddebeer <debeer.dries@gmail.com>
2021-01-14 20:52:49 +0100

    c-method for constriant object

---
+++ R/constraint-methods.R
@@ -0,0 +1,5 @@
+### S3 methods for constraint object
+c.constraint <- function(...){
+  message("'combineConstraints()' is used. Please use 'combineConstraints' directly.")
+  combineConstraints(...)
+}
