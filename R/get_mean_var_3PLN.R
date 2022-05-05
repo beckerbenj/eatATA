ad722708efd8e20dec32296e5b0446a357dd3bdf
ddebeer <debeer.dries@gmail.com>
2022-05-05 15:57:11 +0200

    name change and documentation edits

--- R/get_mean_var_3PLN.R
+++ R/get_mean_var_3PLN.R
@@ -1,22 +1,30 @@
 #############################################################################
-#' Calculate Expected Response Times Moments
+#' Calculate Cumulants Lognormal Response Time Distribution
 #'
-#' Calculate expected response times moments (mean and variance) given item parameters of the log normal response time model.
+#' Calculate the first and second cumulants (i.e., mean and variance) of item
+#' response time distributions given item parameters of the three-parameter
+#' log-normal model (3PLN) for response times.
 #'
-#' Expected response times are calculated according to the log normal response time model by
-#' van der Linden (2006) or Klein Entink et al. (2009). If \code{phi} is \code{1}, the model
-#' by van der Linden (2006) is used. Either a single set of parameters of vectors of each parameter
-#' can be supplied.
+#' Calculate the first and second cumulant of the two-parameter log-normal (2PLN)
+#' model for response times according to van der Linden (2006) or the 3PLN according
+#' to Klein Entink et al. (2009). If the speed sensitivity parameter \code{phi}
+#' in the 3PLN equals \code{1}, the model reduces tot the 2PLN, yet with a
+#' different parameterization for the item specific residual variance \code{sdEpsi}
+#' compared to van der Linden (2006).
+#'
+#' The cumulants are computed for one or more speed parameters, and for one or more
+#' sets of item parameters.
 #'
 #' The calculation is based on Fenton (1960). For the model by van der Linden (2006), the calculation was
 #' first introduced by van der Linden (2011).
 #'
 #'@param lambda Vector of time intensity parameters.
-#'@param phi [optional] Vector of time sensitivity parameters.
+#'@param phi [optional] Vector of speed sensitivity parameters.
 #'@param zeta Vector of person speed parameters.
 #'@param sdEpsi Vector of item specific residual variances.
 #'
-#'@return a matrix, with columns for different \code{zeta} and rows for different items
+#'@return a matrix with either the mean or the variance of the response time distributions,
+#'        with columns for different \code{zeta} and rows for different items
 #'
 #'@references Fenton, L. (1960). The sum of log-normal probability distributions in scatter transmission systems. \emph{IRE
 #' Transactions on Communication Systems}, 8, 57-67.
@@ -34,55 +42,77 @@
 #'
 #'@examples
 #'# expected RT for a single item (van der Linden model)
-#'calculateExpectedRT(lambda = 3.8, zeta = 0, sdEpsi = 0.3)
-#'calculateExpectedRTvar(lambda = 3.8, zeta = 0, sdEpsi = 0.3)
+#'calculateMeanRT(lambda = 3.8, zeta = 0, sdEpsi = 0.3)
+#'get_var_3PLN(lambda = 3.8, zeta = 0, sdEpsi = 0.3)
 #'
 #'# expected RT for multiple items (van der Linden model)
-#'calculateExpectedRT(lambda = c(4.1, 3.8, 3.5), zeta = 0,
+#'get_mean_3PLN(lambda = c(4.1, 3.8, 3.5), zeta = 0,
 #'                    sdEpsi = c(0.3, 0.4, 0.2))
-#'calculateExpectedRTvar(lambda = c(4.1, 3.8, 3.5), zeta = 0,
+#'get_var_3PLN(lambda = c(4.1, 3.8, 3.5), zeta = 0,
 #'                    sdEpsi = c(0.3, 0.4, 0.2))
 #'
 #'# expected RT for multiple items and multiple spped levels (Klein Entink model)
-#'calculateExpectedRT(lambda = c(3.7, 4.1, 3.8), phi = c(1.1, 0.8, 0.5),
+#'get_mean_3PLN(lambda = c(3.7, 4.1, 3.8), phi = c(1.1, 0.8, 0.5),
 #'                     zeta = c(-1, 0, 1), sdEpsi = c(0.3, 0.4, 0.2))
-#'calculateExpectedRTvar(lambda = c(3.7, 4.1, 3.8), phi = c(1.1, 0.8, 0.5),
+#'get_var_3PLN(lambda = c(3.7, 4.1, 3.8), phi = c(1.1, 0.8, 0.5),
 #'                     zeta = c(-1, 0, 1), sdEpsi = c(0.3, 0.4, 0.2))
 #'
-#' @describeIn calculateExpectedRT Calculate expected mean response time
+#' @describeIn get_mean_3PLN Calculate mean 3PLN
 #'@export
-calculateExpectedRT <- function(lambda, phi = rep(1, length(lambda)), zeta, sdEpsi) {
+get_mean_3PLN <- function(lambda, phi = rep(1, length(lambda)), zeta, sdEpsi) {
   if(!is.numeric(lambda)) stop("'lambda' must be a numeric vector.")
   if(!is.numeric(phi)) stop("'phi' must be a numeric vector.")
   if(!is.numeric(zeta)) stop("'zeta' must be a numeric vector.")
   if(!is.numeric(sdEpsi)) stop("'sdEpsi' must be a numeric vector.")
   if(length(sdEpsi) != length(lambda) || length(sdEpsi) != length(phi)) stop("'lambda', 'phi', and 'sdEpsi' must be of the same length.")
 
-  cumulants <- getCumulantRT(zeta, lambda, phi, sdEpsi)
+  cumulants <- get_cumulant_3PLN(zeta, lambda, phi, sdEpsi)
   return(cumulants[[1]])
 }
 
-#' @describeIn calculateExpectedRT Calculate expected response time variance
+#' @describeIn get_mean_3PLN Calculate mean 2PLN
 #'@export
-calculateExpectedRTvar <- function(lambda, phi = rep(1, length(lambda)), zeta, sdEpsi) {
+get_mean_2PLN <- function(lambda, zeta, sdEpsi) {
+  get_mean_3PLN(lambda = lambda,
+                phi = rep(1, length(lambda)),
+                zeta = zeta,
+                sdEpsi = sdEpsi)
+}
+
+#' @describeIn get_mean_3PLN Calculate variance 3PLN
+#'@export
+get_var_3PLN <- function(lambda, phi = rep(1, length(lambda)), zeta, sdEpsi) {
   if(!is.numeric(lambda)) stop("'lambda' must be a numeric vector.")
   if(!is.numeric(phi)) stop("'phi' must be a numeric vector.")
   if(!is.numeric(zeta)) stop("'zeta' must be a numeric vector.")
   if(!is.numeric(sdEpsi)) stop("'sdEpsi' must be a numeric vector.")
   if(length(sdEpsi) != length(lambda) || length(sdEpsi) != length(phi)) stop("'lambda', 'phi', and 'sdEpsi' must be of the same length.")
 
-  cumulants <- getCumulantRT(zeta, lambda, phi, sdEpsi)
+  cumulants <- get_cumulant_3PLN(zeta, lambda, phi, sdEpsi)
   return(cumulants[[2]])
+}
+
+#' @describeIn get_mean_3PLN Calculate variance 2PLN
+#'@export
+get_var_2PLN <- function(lambda, phi = rep(1, length(lambda)), zeta, sdEpsi) {
+  get_var_3PLN(lambda = lambda,
+                phi = rep(1, length(lambda)),
+                zeta = zeta,
+                sdEpsi = sdEpsi)
 }
 
 
 ### Function to compute the first, second and third cumulant of the
 ### item response time function.
+### The first cumulant is equal to the first moment about the origin
+### The second cumulant is equal to the second central moment
+### The third cumulant is equal to the third central moment
+###
 ###   lambda = time intensity parameter (item)
 ###   phi    = speed sensitivity parameter (item)
 ###   zeta   = speed parameter (person)
 ###   sdEpsi = standard deviation of the log response time distribution (item)
-getCumulantRT <- function(zeta, lambda, phi, sdEpsi){
+get_cumulant_3PLN <- function(zeta, lambda, phi, sdEpsi){
   ONEs <- rep(1, length(zeta))
   exponent <- outer(- zeta, phi) + outer(ONEs, (lambda + (sdEpsi^2) / 2))
   # not vectorized: lambda - (zeta * phi) + (sdEpsi^2)/2
